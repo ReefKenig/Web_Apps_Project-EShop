6d09cb440829942f6cdd317ec1b1ef81765cd2d7
Reef Kenig <rkenig@aol.com>
2024-09-09 14:38:49 +0300

    Created users DB model

---
+++ Application/models/users.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  userId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/.+@.+\..+/, "Please enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false, // false = customer, true = admin
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("Password") || this.isNew) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next();
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
