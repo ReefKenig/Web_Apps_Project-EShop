7ef365d96bceaf23ad039459e5375ebb2332bdb3
rajaking1 <davidvow294@gmail.com>
2024-10-05 22:36:55 +0300

    route-manufacturers.and server started

---
+++ Application/routes/manufacturers.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const manufacturerController = require('../controllers/manufacturers');
+
+// Create a new manufacturer
+router.post('/manufacturers', manufacturerController.createManufacturer);
+
+// Get all manufacturers
+router.get('/manufacturers', manufacturerController.getAllManufacturers);
+
+// Get a manufacturer by ID
+router.get('/manufacturers/:manufacturerId', manufacturerController.getManufacturerById);
+
+// Update a manufacturer by ID
+router.put('/manufacturers/:manufacturerId', manufacturerController.updateManufacturer);
+
+// Delete a manufacturer by ID
+router.delete('/manufacturers/:manufacturerId', manufacturerController.deleteManufacturer);
+
+module.exports = router;
