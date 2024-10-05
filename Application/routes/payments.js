10f21424f04a270ec2b6d2ed3ce06450ea2464bf
rajaking1 <davidvow294@gmail.com>
2024-10-05 22:19:13 +0300

    model-cars

---
+++ Application/routes/payments.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const paymentController = require('../controllers/payments');
+
+// Create a new payment
+router.post('/payments', paymentController.createPayment);
+
+//payment status
+router.get("/payments/:paymentId/status",paymentController.checkPaymentStatus);
+
+// Get all payments
+router.get('/payments', paymentController.getAllPayments);
+
+// Get a payment by paymentId
+router.get('/payments/:paymentId', paymentController.getPaymentById);
+
+// Update a payment by paymentId
+router.put('/payments/:paymentId', paymentController.updatePayment);
+
+// Delete a payment by paymentId
+router.delete('/payments/:paymentId', paymentController.deletePayment);
+
+module.exports = router;
