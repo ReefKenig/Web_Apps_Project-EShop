5ec745ab8b1f1181c373922fdc454ec520356391
Reef Kenig <rkenig@aol.com>
2024-10-11 13:35:56 +0300

    Modified car colors behaviour

--- Application/routes/carColors.js
+++ Application/routes/carColors.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const carColorController = require("../controllers/carColors");
 
 // Create a new color
-router.post("/", carColorController.createCarColor);
+router.post("/", carColorController.createColor);
 
 // Get all colors
-router.get("/", carColorController.getAllCarColors);
+router.get("/", carColorController.getAllColors);
 
 // Get a color by ID
 router.get("/:colorId", carColorController.getColorById);
 
 // Update a color
-router.put("/:colorId", carColorController.updateCarColor);
+router.put("/:colorId", carColorController.updateColor);
 
 // Delete a color
-router.delete("/:colorId", carColorController.deleteCarColor);
+router.delete("/:colorId", carColorController.deleteColor);
 
 module.exports = router;
--- Application/server.js
+++ Application/server.js
@@ -6,6 +6,13 @@ const carColorRoutes = require("./routes/carColors");
 dotenv.config();
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/color", carColorRoutes);
+
+// MongoDB connection
 const uri = `mongodb+srv://rkenig:${encodeURIComponent(
   "&f9-&Hy!NvaKvQX"
 )}@carseshop.uecqa.mongodb.net/CarsEShop`;
@@ -13,6 +20,8 @@ const uri = `mongodb+srv://rkenig:${encodeURIComponent(
 async function connect() {
   try {
     await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+    
   } catch (error) {
     console.log(error);
   }
@@ -20,8 +29,7 @@ async function connect() {
 
 connect();
 
-app.use("/color", carColorRoutes);
-
+// Start server
 app.listen(process.env.PORT || 3030, () => {
   console.log(`Server listening on port ${process.env.PORT || 3030}`);
 });
