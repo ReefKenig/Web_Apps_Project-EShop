b882c6a7319e3e1b7412ef4e78a724d74e3e5811
Reef Kenig <rkenig@aol.com>
2024-10-11 14:39:06 +0300

    fixed router

--- Application/routes/carimages.js
+++ Application/routes/carimages.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 const carImageController = require('../controllers/carImages');
 
-router.post('/images', carImageController.addCarImage);
-router.get('/images', carImageController.getCarImages);
-router.put('/images/:carid', carImageController.updateCarImage);
-router.delete('/images/:carid', carImageController.deleteCarImage);
+// Create a new image
+router.post('/', carImageController.addCarImage);
+
+// Get all images
+router.get('/', carImageController.getAllImages);
+
+// Get an image by ID
+router.get("/:id", carImageController.getImageById);
+
+// Update an image
+router.put('/:id', carImageController.updateCarImage);
+
+// Delete an image
+router.delete('/:id', carImageController.deleteCarImage);
 
 module.exports = router;
