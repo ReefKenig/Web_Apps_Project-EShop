d954268f2a1ff34c0a5cb55c5ed0246ad09a09a5
Reef Kenig <rkenig@aol.com>
2024-10-22 11:30:22 +0300

    Deleted duplicate function

--- Application/models/users.js
+++ Application/models/users.js
@@ -36,20 +36,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function (next) {
-  if (this.isModified("Password") || this.isNew) {
-    try {
-      const salt = await bcrypt.genSalt(10);
-      this.password = await bcrypt.hash(this.password, salt);
-      next();
-    } catch (error) {
-      next(error);
-    }
-  } else {
-    return next();
-  }
-});
-
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
