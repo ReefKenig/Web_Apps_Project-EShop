87700dec1584368aef6ff0610ade28f55431a7a8
Reef Kenig <rkenig@aol.com>
2024-10-27 18:38:45 +0200

    Static files can be accessed

--- Application/server.js
+++ Application/server.js
@@ -39,15 +39,7 @@ async function connect() {
 
 connect();
 
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
+app.use(express.static(path.join(__dirname, 'client')));
 
 // Start server
 app.listen(process.env.PORT || 3030, () => {
