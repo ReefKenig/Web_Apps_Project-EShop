e987a57684f3587d9c0d53d2f1a59c4f2c4564fa
rajaking1 <davidvow294@gmail.com>
2024-10-27 20:09:13 +0200

    collection-users

--- Application/routes/user.js
+++ Application/routes/user.js
@@ -3,22 +3,22 @@ const userController = require('../controllers/user');
 const router = express.Router();
 
 // Create a new user
-router.post('/', userController.createUser);
+router.post('/', userController.createUser);//works
 
 // User login
-router.post('/login', userController.userLogin); 
+router.post('/login', userController.userLogin); //works
 
 // Get all users
-router.get('/', userController.getAllUsers);
+router.get('/', userController.getAllUsers);//works
 
 // Get a user by ID
-router.get('/:id', userController.getUserById);
+router.get('/:id', userController.getUserById);//works
 
 // Update a user by ID
-router.put('/:id', userController.updateUser);
+router.put('/:id', userController.updateUser);//works
 
 // Delete a user by ID
-router.delete('/:id', userController.deleteUser);
+router.delete('/:id', userController.deleteUser);//works
 
 // Add an order to a user's order history
 router.post('/:id/order', userController.addOrder);
