0077d7bf8035158d0bbbdc47010b997f35ce07af
rajaking1 <davidvow294@gmail.com>
2024-10-29 10:38:35 +0200

    new users added to cars

---
+++ Application/models/users.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  orderHistory: 
+  [
+    {
+      date: {
+        type: Date,
+        required: true,
+        default: Date.now,
+      },
+      items: [
+        {
+          carId: {
+            type: Schema.Types.objectId,
+            ref: "Car",
+            required: true,
+          },
+          price: {
+            type: Number,
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+      totalPrice: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  shoppingCart: [
+    {
+      brand: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+},
+{collection:"user"},
+);
+
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
---
+++ Application/routes/users.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const userController = require('../controllers/users'); 
+const router = express.Router();
+
+// Create a new user
+router.post('/', userController.createUser);
+
+// User login
+router.post('/login', userController.userLogin); 
+
+// Get all users
+router.get('/', userController.getAllUsers);
+
+// Get a user by ID
+router.get('/:id', userController.getUserById);
+
+// Update a user by ID
+router.put('/:id', userController.updateUser);
+
+// Delete a user by ID
+router.delete('/:id', userController.deleteUser);
+
+// Add an order to a user's order history
+router.post('/order/:id', userController.addOrder);
+
+module.exports = router;
+
