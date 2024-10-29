7086fbd3cbdc3b9698105fa06c33d8c3e4937f2f
rajaking1 <davidvow294@gmail.com>
2024-10-29 10:36:34 +0200

    new users

--- Application/models/users.js
+++ Application/models/users.js
@@ -1,81 +1,41 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
 
-const userSchema = Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  orderHistory: 
-  [
-    {
-      date: {
-        type: Date,
-        required: true,
-        default: Date.now,
-      },
-      items: [
-        {
-          carId: {
-            type: Schema.Types.objectId,
-            ref: "Car",
-            required: true,
-          },
-          price: {
-            type: Number,
-            required: true,
-          },
-          quantity: {
-            type: Number,
-            required: true,
-          },
-        },
-      ],
-      totalPrice: {
-        type: Number,
-        required: true,
-      },
-    },
-  ],
-  shoppingCart: [
-    {
-      brand: {
-        type: String,
-        required: true,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
-    },
-  ],
-},
-{collection:"user"},
-);
+// const userSchema = new mongoose.Schema({
+//   userId: {
+//     type: Number,
+//     required: true,
+//     unique: true,
+//   },
+//   firstName: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   lastName: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     lowercase: true,
+//     trim: true,
+//     match: [/.+@.+\..+/, "Please enter a valid email address"],
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   isAdmin: {
+//     type: Boolean,
+//     required: true,
+//     default: false, // false = customer, true = admin
+//   },
+// });
 
+// const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model('user', userSchema);
-
-module.exports = User;
+// module.exports = User;
