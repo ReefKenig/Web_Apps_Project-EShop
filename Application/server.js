34d7d7bae19ec1380940008e46fb9bd86a2f73c1
Reef Kenig <rkenig@aol.com>
2024-11-04 21:39:52 +0200

    added user router to server

--- Application/server.js
+++ Application/server.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-
 const connect = require("./config/db");
 
 // Routers
 const carRoutes = require("./routes/car");
+const userRoutes = require("./routes.user");
 
 dotenv.config();
 const app = express();
@@ -16,16 +16,12 @@ app.use(express.json());
 
 // Routes
 app.use("/api/car", carRoutes);
+app.use("/api/user", userRoutes);
 
 // MongoDB connection
 const uri = process.env.MONGODB_URI;
 
 connect(uri);
-
-//middleware
-app.use(express.json()); 
-
-app.use("/api/user",userRoutes);
 
 app.listen(process.env.PORT || 3030, () => {
   console.log(`Server listening on port ${process.env.PORT || 3030}`);
