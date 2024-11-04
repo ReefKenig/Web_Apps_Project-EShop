b90c7147cf52b9fc532f9457a342bfe9f071bb04
Reef Kenig <rkenig@aol.com>
2024-11-04 21:02:56 +0200

    upgraded collection

--- Application/routes/users.js
+++ Application/routes/users.js
@@ -1,26 +1,23 @@
-const express = require('express');
-const userController = require('../controllers/users'); 
+const express = require("express");
+const userController = require("../controllers/users");
 const router = express.Router();
 
 // Create a new user
-router.post('/', userController.createUser);
+router.post("/register", userController.register);
 
 // User login
-router.post('/login', userController.userLogin); 
+router.post("/login", userController.login);
 
 // Get all users
-router.get('/', userController.getAllUsers);
+router.get("/", userController.getAllUsers);
 
 // Get a user by ID
-router.get('/:id', userController.getUserById);
+router.get("/:id", userController.getUserById);
 
 // Update a user by ID
-router.put('/:id', userController.updateUser);
+router.put("/:id", userController.updateUser);
 
 // Delete a user by ID
-router.delete('/:id', userController.deleteUser);
-
-
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
-
