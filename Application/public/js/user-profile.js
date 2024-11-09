41adebbbbe5860adf71e8cf254b941afe26c1156
Liron Daniel <lirond48@gmail.com>
2024-11-10 01:44:24 +0200

    fix

--- Application/public/js/user-profile.js
+++ Application/public/js/user-profile.js
@@ -23,31 +23,31 @@ window.onload = function() {
    await getOrderHistory(userInfo.id);
   }
 
-  // function handleUserNameElement(navigationBar) {
-  //   const user = localStorage.getItem("user");
-  //   const userInfo = JSON.parse(user);
-  //   const userNameDiv = document.createElement("div"); 
-  //   userNameDiv.style.cssText = "max-width: 155px;";
-  //   const userNameSpan = document.createElement("span");
-  //   userNameSpan.style.color = '#b79d76';
-  //   userNameSpan.textContent = `Welcome ${userInfo.firstName} ${userInfo.lastName}`
-  //   userNameDiv.appendChild(userNameSpan);
-  //   navigationBar.appendChild(userNameDiv);
-  // }
+  function handleUserNameElement(navigationBar) {
+    const user = localStorage.getItem("user");
+    const userInfo = JSON.parse(user);
+    const userNameDiv = document.createElement("div"); 
+    userNameDiv.style.cssText = "max-width: 155px;";
+    const userNameSpan = document.createElement("span");
+    userNameSpan.style.color = '#b79d76';
+    userNameSpan.textContent = `Welcome ${userInfo.firstName} ${userInfo.lastName}`
+    userNameDiv.appendChild(userNameSpan);
+    navigationBar.appendChild(userNameDiv);
+  }
 
-  // function handleLogoutElement(navigationBar) {
-  //   const registerLi = document.getElementById("register");
-  //   if (registerLi) {
-  //     registerLi.remove();
-  //     const logoutDiv = document.createElement("div"); 
-  //       const logoutButton = document.createElement("button");
-  //       logoutButton.textContent = "Logout";
-  //       logoutButton.classList.add("logout-button");
-  //       logoutButton.onclick = logout;
-  //       logoutDiv.appendChild(logoutButton);
-  //       navigationBar.appendChild(logoutDiv);
-  //   }
-  // }
+  function handleLogoutElement(navigationBar) {
+    const registerLi = document.getElementById("register");
+    if (registerLi) {
+      registerLi.remove();
+      const logoutDiv = document.createElement("div"); 
+        const logoutButton = document.createElement("button");
+        logoutButton.textContent = "Logout";
+        logoutButton.classList.add("logout-button");
+        logoutButton.onclick = logout;
+        logoutDiv.appendChild(logoutButton);
+        navigationBar.appendChild(logoutDiv);
+    }
+  }
 
   async function getOrderHistory(userId) {
     try {
@@ -117,11 +117,6 @@ function renderOrderHistory(data) {
 
 function handleLinksLocation() {
   setTimeout(() => {
-    const token = localStorage.getItem("authToken");
-    if(token) {
-      const register = document.getElementById('register');
-      register.remove();
-    }
     let ul = document.getElementById('ul');
     let li = document.createElement('li');
     let a = document.createElement('a');
@@ -139,6 +134,10 @@ function handleLinksLocation() {
     logo.href = '../../client/index.html';
     shop.href = '../../client/src/primay-page/primary-page.html';
     contact.href = '../../client/src/contact-us/contact-us.html';
+
+    const navigationBar = document.getElementById("nav");
+    handleUserNameElement(navigationBar);
+    handleLogoutElement(navigationBar);
 }, 100);
 }
 
