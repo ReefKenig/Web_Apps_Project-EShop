d2ddb8c7383ebd07bbe94db0857cb17ed02c7c39
Reef Kenig <rkenig@aol.com>
2024-11-09 22:48:12 +0200

    .

--- Application/routes/orders.js
+++ Application/routes/orders.js
@@ -1,29 +1,20 @@
 // routes/orderRoutes.js
 const express = require("express");
 const orderController = require("../controllers/orders");
-const express = require("express");
-const orderController = require("../controllers/orders");
 const router = express.Router();
 
 // Create a new order
 router.post("/", orderController.createOrder);
 
-// Get all orders (Admin view)
-router.get("/", orderController.getAllOrders);
+router.get("/search", orderController.getOrders);
 
 // Get order by ID
 router.get("/:id", orderController.getOrderById);
-
-// Get orders by user ID
-router.get("/user/:id", orderController.getOrdersByUserId);
 
 // Update order details
 router.put("/:id", orderController.updateOrder);
 
 // Delete an order
 router.delete("/:id", orderController.deleteOrder);
-
-// Get total revenue by month for a specific year
-router.get("/revenue/:year", orderController.getRevenueByMonth);
 
 module.exports = router;
